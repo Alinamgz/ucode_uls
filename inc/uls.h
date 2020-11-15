9a7a94236b21e9e58c739f9db9c73e89f56b3329
Alina Mazur <amazur@e3r4p5.unit.ua>
2020-11-15 14:19:43 +0200

    initial commit

---
+++ inc/uls.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// ====== defines ======
+#define HELLO_MSG "Hello World!\n"
+
+// ====== includes ======
+#include "libmx.h"
+
+// ====== structs ======
+
+// ======= funcs =======
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include "uls.h"
+
+//TODO: del .travis.yml before pushing to ucode gitlab!
+
+int main(void) {
+    mx_printstr(HELLO_MSG);
+    return 0;
+}
