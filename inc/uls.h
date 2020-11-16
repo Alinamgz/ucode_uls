b77b49bafdc3c20bef2dc29d9d8d14abc5e62eaf
Alina Mazur <amazur@e3r4p1.unit.ua>
2020-11-16 03:05:41 +0200

    @alinamgz mx_parse_input.c draft to edit

--- inc/uls.h
+++ inc/uls.h
@@ -5,7 +5,16 @@
 
 // ====== includes ======
 #include "libmx.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
 
 // ====== structs ======
+typedef struct s_parce {
+    char *flags;
+    char **addresses;
+}
 
 // ======= funcs =======
--- src/main.c
+++ src/main.c
@@ -2,7 +2,52 @@
 
 //TODO: del .travis.yml before pushing to ucode gitlab!
 
-int main(void) {
-    mx_printstr(HELLO_MSG);
+static inline bool is_flag(const char *str) {
+    bool rslt = str[0] == '-' && (mx_strlen(str) > 1) ? 1 : 0;
+    return rslt;
+}
+
+int main(int argc, char **argv) {
+    //char *flg = NULL;
+    char *flags_draft = NULL;
+    char **files = NULL;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    if (argc == 1) {
+        mx_printstr(HELLO_MSG);
+    }
+    else {
+        if (is_flag(argv[1])) {
+            for(j = 1; j < argc && is_flag(argv[j]); j++) {
+                //flg = argv[j];
+                flags_draft = mx_strjoin(flags_draft, argv[j] + 1);
+            }
+        }
+
+        files = (char**)malloc(sizeof(char*) * argc);
+        for (i = 0; i <= argc; i++) {
+            files[i] = NULL;
+        }
+        for(; j < argc; j++) {
+            files[k++] = mx_strdup(argv[j]);
+        }
+    }
+
+    if (flags_draft) {
+        mx_printstr("\n\n=========== flags ==============\n");
+            mx_printstr(flags_draft);
+        }
+        if (files) {
+            mx_printstr("\n\n=========== files ==============\n");
+            for(i = 0; files[i]; i++) {
+                mx_printchar('\t');
+                mx_printstr(files[i]);
+            }
+
+        }
+mx_printstr("\ncheck\n");
+
     return 0;
 }
