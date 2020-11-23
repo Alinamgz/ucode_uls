af7b44f9b46409beeddfea962fa23002490bc496
Alina Mazur <amazur@e1r10p3.unit.ua>
2020-11-23 21:42:49 +0200

    fixed colorizing instuctions for ls (NB! no -l flag) in case output is NOT stdout

--- src/list_directory.c
+++ src/list_directory.c
@@ -1,15 +1,19 @@
 #include "uls.h"
 
-static void column_output(t_parse *p, bool flag_G) {
+static void column_output(t_parse *p, int isatty, bool flag_G) {
     struct stat forstat;
 
     for (int i = 0; i < p->count_of_objects; i++) {
-        if (flag_G){
+        if (isatty && flag_G){
             lstat(p->content_of_directory[i], &forstat);
             mx_colorize(forstat);
+            mx_printstr(p->content_of_directory[i]);
+            mx_printstr(RESET_COLORS);
         }
-        mx_printstr(p->content_of_directory[i]);
-        mx_printstr(RESET_COLORS);
+        else {
+            mx_printstr(p->content_of_directory[i]);
+
+        }
         mx_printchar('\n');
     }
 }
@@ -19,8 +23,10 @@ void list_directory(t_parse *p, char *path, t_flags *f) {
     if (isatty(1) && !f->lg_1) {
         line_output(p, f->lg_G);
     }
-    else
-        column_output(p, f->lg_G);
+    else{
+        column_output(p, isatty(1), f->lg_G);
+    }
+    
     p->content_of_directory = NULL;
     p->count_of_objects = 0;
     return;
