b3c57fbb35b87bbce3670f3b99f68c26609b11a3
Vladyslav Babak <vbabak@e3r2p7.unit.ua>
2020-11-24 22:54:31 +0200

    fix_dev

--- src/mx_count_maxlen_manydirs.c
+++ src/mx_count_maxlen_manydirs.c
@@ -8,30 +8,41 @@ void mx_count_maxlen_manydirs(char *path, t_parse *p, t_forlong *forlong) {
     forlong->major = 0;
     forlong->minor = 0;
     forlong->count_of_device_files = 0;
+    forlong->fault_groupid = NULL;
     struct stat forstat;
     struct passwd *foruserid;
     struct group *forgroupid;
     char *fullpath = NULL;
-    
+
     for (int j = 0; j < 7; j++)
         forlong->max_len[j] = 0;
     for (int i = 0; i < p->count_of_objects; i++) {
         fullpath = NULL;
         fullpath = mx_fullpath(path, p->content_of_directory[i]);
         lstat(fullpath, &forstat);
+
         forlong->max_len[4] += forstat.st_blocks;
         if (mx_intlen(forstat.st_nlink) > forlong->max_len[0]) 
             forlong->max_len[0] = mx_intlen(forstat.st_nlink);
         foruserid = getpwuid(forstat.st_uid);
         if (mx_strlen(foruserid->pw_name) > forlong->max_len[1])
             forlong->max_len[1] = mx_strlen(foruserid->pw_name);
+
         forgroupid = getgrgid(forstat.st_gid);
-        if (mx_strlen(forgroupid->gr_name) > forlong->max_len[2])
-            forlong->max_len[2] = mx_strlen(forgroupid->gr_name);
+        if (forgroupid == NULL) {
+            forlong->fault_groupid = mx_itoa(forstat.st_gid);
+            if (mx_strlen(forlong->fault_groupid) > forlong->max_len[2])
+                forlong->max_len[2] = mx_strlen(forlong->fault_groupid);
+            free(forlong->fault_groupid);
+            forlong->fault_groupid = NULL;
+        }
+        else
+            if (mx_strlen(forgroupid->gr_name) > forlong->max_len[2])
+                forlong->max_len[2] = mx_strlen(forgroupid->gr_name);
 
         if (mx_intlen(forstat.st_size) > forlong->max_len[3])
             forlong->max_len[3] = mx_intlen(forstat.st_size);
-
+            
         if (S_ISCHR(forstat.st_mode) || S_ISBLK(forstat.st_mode)) {
             //mx_find_major_minor(forstat.st_rdev, forlong);
             //if (mx_intlen(forlong->major) > forlong->max_len[5])
